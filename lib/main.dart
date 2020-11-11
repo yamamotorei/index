bb3f716ba419cb05f512cc22447582b68531cc33
“yamamotorei” <23iammvp@gmail.com>
2020-11-12 01:31:23 +0900

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,48 @@
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: ListView.builder(
+        itemCount: 10000,
+        itemBuilder: (context, index) {
+          if ((index + 1)~/ 10 % 2 == 0  ) {
+            return Text(("uni") * ((index + 1) % 10));
+          } else {
+            return Text(("uni") * (10 - (index + 1) % 10));
+          }
+        },
+      ),
+    );
+  }
+}
